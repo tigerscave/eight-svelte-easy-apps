22799a1e31484bcaa891322c75258fbe72ed429f
yuto <y.yatsumimi@gmail.com>
2023-06-26 17:35:36 +0900

    feat(clanet-mock):only appearance

---
+++ src/routes/clanet-mock/panel.svelte
@@ -0,0 +1,132 @@
+<script>
+  let activeRed = false;
+  let activeBlue = false;
+
+  function switchBreakOn () {
+    activeRed = !activeRed;
+    activeBlue = !activeBlue; 
+  }  
+  function switchBreakOff () {
+    activeBlue = !activeBlue;
+    activeRed = !activeRed;
+  }
+</script>
+
+<div class="handleControlPanel">
+  <div class="handleControlContainer">
+    旋回ブレーキ
+    <span class="handleControlSwitch-ON">
+      <button type="button" on:click={switchBreakOn} class={!activeRed ? 'activeRed' : ''}>ON</button>
+      <button type="button" on:click={switchBreakOff} class={activeBlue ? 'activeBlue' : ''}>OFF</button>
+    </span>
+  </div>
+  <div class="handleControlContainer">
+    アイドル&shy;ボリューム
+    <span class="handleControlSwitch-SP">
+      <button type="button" class="switch-up">↑</button>
+      <button type="button" class="switch-down">↓</button>
+    </span>
+  </div>
+  <div id="rotation-free-button" class="handleControlContainer">
+    旋回フリー
+    <span class="handleControlSwitch">
+      <button type="button" >ON</button>
+      <button type="button" >OFF</button>
+    </span>
+  </div>
+  <div class="handleControlContainer">
+    ストップ&shy;スイッチ
+    <span class="handleControlSwitch">
+      <button type="button" >ON</button>
+      <button type="button" >OFF</button>
+    </span>
+  </div>
+  <div class="handleControlContainer">
+    M高速巻下
+    <span class="handleControlSwitch">
+      <button type="button" >ON</button>
+      <button type="button" >OFF</button>
+    </span>
+  </div>
+  <div class="handleControlContainer">
+    AML解除
+    <span class="handleControlSwitch">
+      <button type="button" >ON</button>
+      <button type="button" >OFF</button>
+    </span>
+  </div>
+  <div class="handleControlContainer">
+    S高速巻下
+    <span class="handleControlSwitch">
+      <button type="button" >ON</button>
+      <button type="button" >OFF</button>
+    </span>
+  </div>
+  <div class="handleControlContainer">
+    巻き過&nbsp;&emsp;解除
+    <div class="circle"><span>解除</span></div>
+  </div>
+</div>
+
+<style>
+  .handleControlPanel {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+
+.handleControlContainer {
+  margin-top: 1.8em;
+  margin-left: 1.5em;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.circle {
+  position: absolute;
+  left: 39rem;
+  width: 60px;
+  height: 60px;
+  background: white;
+  border: 1px solid black;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.circle span {
+  writing-mode: vertical-rl;
+  color: black;
+}
+
+.handleControlSwitch {
+  margin-right: 6%;
+  font-size: 1rem;
+  display: flex;
+}
+
+.handleControlSwitch-ON, .handleControlSwitch-SP {
+  margin-right: 6%;
+  font-size: 1rem;
+  display: flex;
+}
+
+button, .switch-on,.switch-off,.switch-up,
+.switch-down  {
+  width: 4.6rem;
+  height: 2.3rem;
+  margin: 0;
+  background: white;
+  border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.activeRed {
+  background-color: red;
+}
+.activeBlue {
+  background-color: blue;
+}
+</style>
---
+++ src/routes/clanet-mock/settings.svelte
@@ -0,0 +1,119 @@
+<!--カスタム設定編集ボタン-->
+<div class="changeSettingsContainer">
+  <div>
+    <p>レバー設定</p>
+  </div>
+  <div>
+    <p>レバー２</p>
+    <select id="lever-list-2" class="groups-select-item" disabled>
+      <option value="未選択">未選択</option>
+      <option value="伸縮">伸縮</option>
+      <option value="S">S</option>
+      <option value="M">M</option>
+      <option value="起伏">起伏</option>
+    </select>
+  </div>
+  <div>
+    <p>レバー３</p>
+    <select id="lever-list-3" class="groups-select-item" disabled>
+      <option value="未選択">未選択</option>
+      <option value="伸縮">伸縮</option>
+      <option value="S">S</option>
+      <option value="M">M</option>
+      <option value="起伏">起伏</option>
+    </select>
+  </div>
+  <div>
+    <p>レバー４</p>
+    <select id="lever-list-4" class="groups-select-item" disabled>
+      <option value="未選択">未選択</option>
+      <option value="伸縮">伸縮</option>
+      <option value="S">S</option>
+      <option value="M">M</option>
+      <option value="起伏">起伏</option>
+    </select>
+  </div>
+  <div>
+    <p>レバー５</p>
+    <select id="lever-list-5" class="groups-select-item" disabled>
+      <option value="未選択">未選択</option>
+      <option value="伸縮">伸縮</option>
+      <option value="S">S</option>
+      <option value="M">M</option>
+      <option value="起伏">起伏</option>
+    </select>
+  </div>
+</div>
+<div class="changeSettingsContainer">
+  <div>
+    <p>ペダル設定</p>
+  </div>
+  <div>
+    <p>ペダルA</p>
+    <select id="pedal-list-A" class="groups-select-item" disabled>
+      <option value="未選択">未選択</option>
+      <option value="S">S</option>
+      <option value="伸縮">伸縮</option>
+    </select>
+  </div>
+  <div>
+    <p>ペダルB</p>
+    <select id="pedal-list-B" class="groups-select-item" disabled>
+      <option value="未選択">未選択</option>
+      <option value="S">S</option>
+      <option value="伸縮">伸縮</option>
+    </select>
+  </div>
+  <div>
+    <p>ペダルC</p>
+    <select id="pedal-list-C" class="groups-select-item" disabled>
+      <option value="未選択">未選択</option>
+      <option value="S">S</option>
+      <option value="伸縮">伸縮</option>
+    </select>
+  </div>
+</div>
+<div class="save-box">
+  <button type="button" class="edit-settings" id="edit-button"
+    >カスタム設定を編集</button
+  >
+</div>
+
+<style>
+  .save-box,
+  .edit-settings {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .edit-settings {
+    width: 11rem;
+    height: 2.3rem;
+    margin-top: 2rem;
+    background: white;
+    border: 1px solid black;
+  }
+  button:active {
+    transform: translateY(1px);
+  }
+
+  .changeSettingsContainer {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .groups-select-item {
+    width: 6.2rem;
+    height: 2.3rem;
+  }
+
+  .edit-settings,
+  .groups-select-item {
+    cursor: pointer;
+  }
+
+  /* #settingsPanel {
+  display: none;
+} */
+</style>
